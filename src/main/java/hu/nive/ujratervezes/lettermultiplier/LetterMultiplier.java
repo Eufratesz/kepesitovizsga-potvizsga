3ee8e65c971edd953786aed6f613da18cb602de4
Eufratesz <lokoshazi@freemail.hu>
2021-06-29 11:28:20 +0200

    Ready

--- src/main/java/hu/nive/ujratervezes/lettermultiplier/LetterMultiplier.java
+++ src/main/java/hu/nive/ujratervezes/lettermultiplier/LetterMultiplier.java
@@ -1,33 +1,30 @@
 package hu.nive.ujratervezes.lettermultiplier;
-/*
-Hozd létre a LetterMultiplier osztályt, és annak a multiply(String input, int number) metódusát! A metódus feladata,
-hogy a paraméterben kapott String minden betűjét többszörözze meg annyi darabra, amit számként paraméterben kapott,
-és ezt a Stringet adja vissza.
 
-Azaz ha a bemeneti string java, az egész szám pedig 3, akkor a kimeneti string jjjaaavvvaaa,
 
-Ha az inputban kapott String üres String, vagy null, vagy a számként megkapott paraméter kisebb, mint 0,
-dobjon IllegalArgumentException-t.
- */
+import java.util.ArrayList;
+import java.util.List;
 
-//public class LetterMultiplier {
-//    public String multiply(String input, int number) {
-//        if (input == null || input.equals("")) {
-//            throw new IllegalArgumentException("no word is given!");
-//        }
-//        if (number< 0 ){
-//                throw new IllegalArgumentException("Invaid number!");
-//            }
-//
-//            StringBuilder sb = new StringBuilder();
-//
-//
-//        for(char c : input.toCharArray()){
-//            c.
-//        }
-//
-//
-//
-//            return sb.toString();
-//        }
-//    }
+public class LetterMultiplier {
+    public String multiply(String input, int number) {
+        if (input == null || input.equals("")) {
+            throw new IllegalArgumentException("no word is given!");
+        }
+        if (number < 0) {
+            throw new IllegalArgumentException("Invaid number!");
+        }
+        String result = "";
+        List<String> newWord = new ArrayList<>();
+
+        for (int i = 0; i < input.length(); i++) {
+
+            String letter = input.substring(i, i + 1);
+            newWord.add(letter);
+
+            result = letter.repeat(number);
+        }
+        return result;
+    }
+
+}
+
+
